5f8c94f53727122e4699bcafef6d098113e11b77
Karim <k.mehour@gmail.com>
2023-01-11 23:01:41 -0500

    Calcule f5 is funcctional

--- calcule_f5.py
+++ calcule_f5.py
@@ -36,7 +36,7 @@ class IndiceF5(QgsProcessingAlgorithm):
         self.addParameter(QgsProcessingParameterNumber('ratio', 'Ratio', optional=True, type=QgsProcessingParameterNumber.Double, minValue=1, maxValue=5, defaultValue=2.5))
         self.addParameter(QgsProcessingParameterVectorLayer('rivnet', 'RivNet', types=[QgsProcessing.TypeVectorLine], defaultValue=None))
         self.addParameter(QgsProcessingParameterNumber('transectsegment', 'Transect/segment', optional=True, type=QgsProcessingParameterNumber.Integer, minValue=1, maxValue=100, defaultValue=10))
-        #self.addParameter(QgsProcessingParameterFeatureSink('Norm', 'Norm', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('sink', 'Sink', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         #self.addParameter(QgsProcessingParameterFeatureSink('Points', 'Points', type=QgsProcessing.TypeVectorPoint, createByDefault=True, supportsAppend=True, defaultValue=None))
 
     def processAlgorithm(self, parameters, context, model_feedback):
@@ -47,7 +47,7 @@ class IndiceF5(QgsProcessingAlgorithm):
         outputs = {}
         tmp = {
             'points':Ntf(suffix="pts.gpkg"),
-            'normals':Ntf(suffix="normals"),
+            'normals':Ntf(suffix="normals.gpkg"),
         }
         
         # Define source stream net
@@ -56,17 +56,20 @@ class IndiceF5(QgsProcessingAlgorithm):
         # Define Sink fields
         sink_fields = source.fields()
         sink_fields.append(QgsField("Indice F5", QVariant.Int))
-        """
+
         # Define sink
         (sink, dest_id) = self.parameterAsSink(
             parameters,
-            'Norm',
+            'sink',
             context,
             sink_fields,
             source.wkbType(),
             source.sourceCrs()
         )
-        """
+
+        # feature count for feedback
+        feature_count = source.featureCount()
+        
         # For each segment
         for segment in source.getFeatures():
             # Materialize river feature
@@ -88,13 +91,16 @@ class IndiceF5(QgsProcessingAlgorithm):
                 'END_OFFSET': 0,
                 'INPUT': single_segment,
                 'START_OFFSET': 0,
-                'OUTPUT':QgsProcessing.TEMPORARY_OUTPUT
-                #'OUTPUT': tmp['points'].name,
+                #'OUTPUT':QgsProcessing.TEMPORARY_OUTPUT
+                'OUTPUT': tmp['points'].name,
                 #'OUTPUT': parameters['Points']
             }
             outputs['PointsAlongGeometry'] = processing.run('native:pointsalonglines', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+            
+            
             # Take ownership of child temporary layer
-            points = context.takeResultLayer(outputs['PointsAlongGeometry']['OUTPUT'])
+            #points = context.takeResultLayer(outputs['PointsAlongGeometry']['OUTPUT'])
+            points = QgsVectorLayer(tmp['points'].name, 'points', 'ogr')
             outputs['PointsAlongGeometry']['OUTPUT'] = points
             
             """
@@ -115,49 +121,68 @@ class IndiceF5(QgsProcessingAlgorithm):
             alg_params = {
                 'EXPRESSION':f"with_variable('len',overlay_nearest(\'{parameters['ptref_widths']}\',Largeur_mod)[0] *  {parameters['ratio']},extend(make_line($geometry,project($geometry,@len,radians(\"angle\" - 90))),@len,0))",
                 #'EXPRESSION':"
-                'INPUT': outputs['PointsAlongGeometry']['OUTPUT'],
-                #'INPUT': tmp['points'].name,
+                #'INPUT': outputs['PointsAlongGeometry']['OUTPUT'],
+                'INPUT': points,
                 'OUTPUT_GEOMETRY': 1,  # Line
                 'WITH_M': False,
                 'WITH_Z': False,
-                #'OUTPUT': tmp['normals'].name
-                'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+                'OUTPUT': tmp['normals'].name
+                #'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
                 #'OUTPUT':parameters['Norm']
             }
             outputs['GeometryByExpression'] = processing.run('native:geometrybyexpression', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
             # Take ownership of child temporary layer
-            normals = context.takeResultLayer(outputs['GeometryByExpression']['OUTPUT'])
+            #normals = context.takeResultLayer(outputs['GeometryByExpression']['OUTPUT'])
+            normals = QgsVectorLayer(tmp['normals'].name, 'normals', 'ogr')
             outputs['GeometryByExpression']['OUTPUT'] = normals
             
-            print(normals.featureCount())
-            print("Valid :", normals.isValid())
-            
+            occurence = dict.fromkeys([0, 2, 3, 4, 5], 0)
+            division_num = 0
             
             for normal in normals.getFeatures():
-                #Evaluating an expression
-                print("bande_riveraine_polly :", parameters["bande_riveraine_polly"])
+                # retreive segment width
+                section_width= normal.geometry().length() / parameters['ratio']
+                
+                #Evaluating intersection distance
                 expr = QgsExpression(
-                f"max(0,length(\n\tsegments_to_lines(\n\t\tintersection(\n\t\t\t$geometry,\n\t\t\tcollect_geometries(\n\t\t\t\t overlay_intersects(\n\t\t\t\t\t@bande_riveraine_polly,\n\t\t\t\t\t$geometry\n\t\t\t\t)\n\t\t\t)\n\t\t)\n\t)\n))",
+                f"max(0,length(segments_to_lines(intersection($geometry,collect_geometries(overlay_intersects('{parameters['bande_riveraine_polly']}',$geometry))))))",
                 )
                 feat_context = QgsExpressionContext()
-                feat_context.setFeature(point)
-                print("Evaluated Expression : ", expr.evaluate(feat_context))
-            
-            """
-            # Field calculator
-            alg_params = {
-                'FIELD_LENGTH': 9,
-                'FIELD_NAME': 'intersect_length',
-                'FIELD_PRECISION': 3,
-                'FIELD_TYPE': 0,  # Decimal (double)
-                'FORMULA': 'max(\n0,\nlength(\n\tsegments_to_lines(\n\t\tintersection(\n\t\t\t$geometry,\n\t\t\tcollect_geometries(\n\t\t\t\t overlay_intersects(\n\t\t\t\t\t@bande_riveraine_polly,\n\t\t\t\t\t$geometry\n\t\t\t\t)\n\t\t\t)\n\t\t)\n\t)\n))',
-                'INPUT': outputs['GeometryByExpression']['OUTPUT'],
-                'OUTPUT': parameters['Norm']
-            }
-            outputs['FieldCalculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-            results['Norm'] = outputs['FieldCalculator']['OUTPUT']
-        """
+                feat_context.setFeature(normal)
+                intersect_len = expr.evaluate(feat_context)
+                
+                if intersect_len >= 2 * section_width:
+                    occurence[0] += 1
+                if intersect_len >= section_width:
+                    occurence[2] += 1
+                if intersect_len >= 0.5 * section_width:
+                    occurence[3] += 1
+                if intersect_len >= 0.5 * section_width:
+                    occurence[4] += 1
+                if intersect_len < 0.5 * section_width:
+                    occurence[5] += 1
+                
+                division_num += 1
+                
+            # Determin the IQM Score
+            if occurence[0] / division_num >= 0.9:
+                indiceF5 = 0
+            elif occurence[2] / division_num >= 0.66:
+                indiceF5 = 2
+            elif occurence[3] / division_num >= 0.66:
+                indiceF5 = 3
+            elif occurence[4] / division_num >= 0.33:
+                indiceF5 = 4
+            else:
+                indiceF5 = 5
         
+            #Write Index
+            segment.setAttributes(
+                segment.attributes() + [indiceF5]
+            )
+            # Add a feature to sink
+            sink.addFeature(segment, QgsFeatureSink.FastInsert)
+            print(f"{segment.id()} / {feature_count}")
         
         #Clear temporary files
         for temp in tmp.values():
