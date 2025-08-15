2a6ebd7c3a1b93e040012bf1c8fe7e90e1a74028
Samuel A. Côté <samuel.cote7a@gmail.com>
2025-08-15 11:27:36 -0400

    Fix : F1 script unexpected behavior
    Fix the F1 score script to comply with the desired behavior. Change the system to identify upstream structures by iterating over the structures to find the related river segment and then find its downstream segment. Evaluate the distance between the structure and the downstream segment by finding the shortest path on the river network between the two. Identify if the distance is less than 1000 m to increment a structure counter for the downstream segment. Correct the index calculation by only taking into account the number of upstream structures (within 1000 m)
    
    Closes : Issue #26

--- Indicateurs_IQM/calcul_f1.py
+++ Indicateurs_IQM/calcul_f1.py
@@ -1,6 +1,7 @@
 
 from qgis.PyQt.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsProcessing,
+                       QgsProcessingContext,
                        QgsFeatureSink,
                        QgsField,
                        QgsProcessingException,
@@ -8,13 +9,11 @@ from qgis.core import (QgsProcessing,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterVectorLayer,
                        QgsProcessingParameterFeatureSink,
-                       QgsExpression,
-                       QgsExpressionContext,
-                       QgsExpressionContextUtils,
+                       QgsWkbTypes,
                        QgsVectorLayer,
-                       QgsProject,
+                       QgsFeature,
                        QgsFeatureRequest,
-                       QgsSpatialIndex
+                       QgsGeometry
                        )
 from qgis import processing
 
@@ -22,8 +21,6 @@ from qgis import processing
 class IndiceF1(QgsProcessingAlgorithm):
     INPUT = 'INPUT'
     OUTPUT = 'OUTPUT'
-    STRUCTURES_PATH = '/home/karim/uqac/indice_F1/data/Structure_tq_shp/gsq_v_desc_strct_tri_rpr.shp'
-    FID = "Id"
 
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT, self.tr('Réseau hydrographique (CRHQ)'), [QgsProcessing.TypeVectorLine]))
@@ -31,7 +28,6 @@ class IndiceF1(QgsProcessingAlgorithm):
         self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT, self.tr('Couche de sortie')))
 
     def processAlgorithm(self, parameters, context, model_feedback):
-        outputs = {}
         source = self.parameterAsSource(
             parameters,
             self.INPUT,
@@ -45,6 +41,7 @@ class IndiceF1(QgsProcessingAlgorithm):
         #Adding new field to output
         sink_fields = source.fields()
         sink_fields.append(QgsField("Indice F1", QVariant.Int))
+        sink_fields.append(QgsField("Nb_struct_amont", QVariant.Int))
 
         (sink, dest_id) = self.parameterAsSink(
             parameters,
@@ -60,34 +57,90 @@ class IndiceF1(QgsProcessingAlgorithm):
 
         # Send some information to the user
         model_feedback.pushInfo('CRS is {}'.format(source.sourceCrs().authid()))
+ 
+        structure_counts = {}
+        struct_layer = self.parameterAsVectorLayer(parameters, 'structs', context)
+        hydro_layer = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+
+
         # Gets the number of features to iterate over for the progress bar
-        total_features = source.featureCount()
+        total_features = struct_layer.featureCount()
         model_feedback.pushInfo(self.tr(f"\t {total_features} features à traiter"))
 
-        for current, feature in enumerate(source.getFeatures()):
+        try :
+            for current, struct in enumerate(struct_layer.getFeatures()):
+                current_feat = None
+                try :
+                    # Finds the river segment of the current structure
+                    current_feat = find_segment_for_structure(struct, hydro_layer, context)
+                except Exception as e :
+                    model_feedback.reportError(self.tr(f"Erreur dans find_segment_for_structure : {str(e)}"))
+                if current_feat is None:
+                    continue
 
-            if model_feedback.isCanceled():
+                downstream_feat = None
+                try :
+                    # Finds the downstream river segment
+                    downstream_feat = get_downstream_segment(hydro_layer, current_feat)
+                except Exception as e :
+                    model_feedback.reportError(self.tr(f"Erreur dans get_downstream_segment : {str(e)}"))
+                if downstream_feat is None:
+                    continue
+
+                intersection_point = None
+                try :
+                    # Find the intersecting point between the structure river segment and the downstream segment
+                    intersection_point = get_intersection_point(current_feat, downstream_feat)
+                except Exception as e :
+                    model_feedback.reportError(self.tr(f"Erreur dans get_intersection_point : {str(e)}"))
+                if intersection_point is None:
+                    continue
+
+                try :
+                    # Calculates the distance along the network between the structure and this point
+                    cost = compute_shortest_path(struct, intersection_point, hydro_layer, model_feedback, context)
+                except Exception as e :
+                    model_feedback.reportError(self.tr(f"Erreur dans compute_shortest_path : {str(e)}"))
+
+                # If the distance is < 1000 m, increment the downstream segment structure counter.
+                if cost is None :
+                    continue
+                if cost < 1000:
+                    downstream_id = downstream_feat['Id_UEA']
+                    structure_counts[downstream_id] = structure_counts.get(downstream_id, 0) + 1
+
+                # Updating the progress bar
+                if total_features != 0:
+                    progress = int(100*(current/total_features))
+                else:
+                    progress = 0
+                model_feedback.setProgress(progress)
+
+                if model_feedback.isCanceled():
+                    return {}
+        except Exception as e :
+            model_feedback.reportError(self.tr(f"Erreur dans la boucle de structure : {str(e)}"))
+
+        model_feedback.setProgressText(self.tr(f"Compte des structures terminé."))
+        if model_feedback.isCanceled():
+            return {}
+
+        # Computing the F1 score for each river segment
+        try :
+            for feat in source.getFeatures():
+                seg_id = feat['Id_UEA']
+                struct_count = structure_counts.get(seg_id, 0)
+                f1_score = computeF1(struct_count)
+
+                # add both the structure count and the f1_score to the attributes table
+                feat.setAttributes(feat.attributes() + [f1_score, struct_count])
+                sink.addFeature(feat, QgsFeatureSink.FastInsert)
+        except Exception as e :
+            model_feedback.reportError(self.tr(f"Erreur dans le calcul de F1 et le sink des features : {str(e)}"))
+
+        model_feedback.setProgressText(self.tr(f"Calcul du score de F1 terminé."))
+        if model_feedback.isCanceled():
                 return {}
-
-            buffer = gen_buffer(feature, source, context=context)
-            struct_count = count_structures(buffer, parameters)
-
-            indiceF1 = computeF1(feature, struct_count)
-
-            feature.setAttributes(
-                feature.attributes() + [indiceF1]
-            )
-
-            # Increments the progress bar
-            if total_features != 0:
-                progress = int(100*(current/total_features))
-            else:
-                progress = 0
-            model_feedback.setProgress(progress)
-            model_feedback.setProgressText(self.tr(f"Traitement de {current} segments sur {total_features}"))
-
-            # Add a feature in the sink
-            sink.addFeature(feature, QgsFeatureSink.FastInsert)
 
         # Ending message
         model_feedback.setProgressText(self.tr('\tProcessus terminé !'))
@@ -98,20 +151,26 @@ class IndiceF1(QgsProcessingAlgorithm):
     def tr(self, string):
         return QCoreApplication.translate('Processing', string)
 
+
     def createInstance(self):
         return IndiceF1()
+
 
     def name(self):
         return 'indicef1'
 
+
     def displayName(self):
         return self.tr('Indice F1')
+
 
     def group(self):
         return self.tr('IQM (indice solo)')
 
+
     def groupId(self):
         return 'iqm'
+
 
     def shortHelpString(self):
         return self.tr(
@@ -128,39 +187,88 @@ class IndiceF1(QgsProcessingAlgorithm):
             "-> Réseau hydrographique du bassin versant avec le score de l'indice F1 calculé pour chaque UEA."
         )
 
-def evaluate_expression(expression_str, vlayer, feature=None ):
-    expression = QgsExpression(expression_str)
-    context = QgsExpressionContext()
-    if feature:
-        context.setFeature(feature)
-    scopes = QgsExpressionContextUtils.globalProjectLayerScopes(vlayer)
-    context.appendScopes(scopes)
-    res = expression.evaluate(context)
-    return res
 
-def gen_buffer(feature, vlayer, context, feedback=None, parameters={}):
-    segment = vlayer.materialize(QgsFeatureRequest().setFilterFids([feature.id()]))
-    # Buffering subsegments
-    alg_params = {'INPUT':segment,'DISTANCE':1000 ,'SEGMENTS':5,'END_CAP_STYLE':0,'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':True,'OUTPUT':'TEMPORARY_OUTPUT'}
-    buffer = processing.run("native:buffer", alg_params, context=context, is_child_algorithm=True)['OUTPUT']
-    return context.takeResultLayer(buffer)
+def find_segment_for_structure(structure, hydro_layer, context, distance=5):
+    # Use the QGIS processing to select river segments located within `distance` meters of the structure point.
+    # Create a temporary layer with the structure point
+    point_layer = QgsVectorLayer("Point?crs=" + hydro_layer.crs().authid(), "structure_point", "memory")
+    provider = point_layer.dataProvider()
+    feat = QgsFeature()
+    feat.setGeometry(structure.geometry())
+    provider.addFeatures([feat])
+    point_layer.updateExtents
 
-def count_structures(buffer, parameters):
-    expr_str = f"""
-    array_length(overlay_intersects('{parameters['structs']}', @geometry))
-    """
-    feature = next(buffer.getFeatures())
-    count = evaluate_expression(expr_str, buffer, feature=feature)
-    #print(count)
-    return count
+    alg_params = {
+        'INPUT': hydro_layer,
+        'REFERENCE': point_layer,
+        'DISTANCE': distance,
+        'METHOD': 0 # 0, create a new selection
+    }
+    processing.run("native:selectwithindistance", alg_params, context=context, is_child_algorithm=True)
+    selected_feats = list(hydro_layer.getSelectedFeatures())
+    # Returns the first segment found (the closest one)
+    return selected_feats[0] if selected_feats else None
 
-def computeF1(feature, struct_count):
-    length = feature.geometry().length()
-    if not length or not struct_count:
+
+def get_downstream_segment(hydro_layer, current_feat):
+    # Retrieves the ID of the downstream segment from the attribute field.
+    downstream_id = current_feat['Id_UEA_aval']
+    # Search for the corresponding segment in the layer
+    request = QgsFeatureRequest().setFilterExpression(f'"Id_UEA" = \'{downstream_id}\'')
+    return next(hydro_layer.getFeatures(request), None)
+
+
+def get_intersection_point(feat1, feat2):
+    # Calculates the geometric intersection between the two segments
+    intersection = feat1.geometry().intersection(feat2.geometry())
+    # If the intersection is a point, return it
+    if intersection and intersection.type() == QgsWkbTypes.PointGeometry:
+        return intersection
+    # If it's a multipoint, we take the first one.
+    elif intersection and intersection.type() == QgsWkbTypes.MultiPointGeometry:
+        return intersection.asMultiPoint()[0]
+    # Otherwise, we return None
+    return None
+
+
+def compute_shortest_path(structure_point, target_point, hydro_layer, feedback, context):
+    # Extract the coordinates for the points
+    start_coords = structure_point.geometry().asPoint()
+    end_coords = target_point.asPoint()
+
+    # Find the shortest path on the river network between the downstream segment and the given structure to get the distance between the two
+    alg_params = {
+        'INPUT': hydro_layer,
+        'STRATEGY': 0,  # 0 = shortest path
+        'START_POINT': f"{start_coords.x()},{start_coords.y()}",
+        'END_POINT': f"{end_coords.x()},{end_coords.y()}",
+        'DEFAULT_DIRECTION': 2,
+        'DEFAULT_SPEED': 1,
+        'TOLERANCE': 5,
+        'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+    }
+    result = processing.run("native:shortestpathpointtopoint", alg_params, context=context, is_child_algorithm=True)
+    temp_layer = context.takeResultLayer(result['OUTPUT'])
+
+    if not temp_layer.isValid():
+        feedback.reportError("La couche temporaire n'est pas valide.")
+        return None
+    if 'cost' not in temp_layer.fields().names():
+        feedback.reportError("Le champ 'cost' est introuvable dans la couche de chemin.")
+        return None
+
+    path_feat = next(temp_layer.getFeatures(), None)
+    # Return the length of the shortest path on the river network ('cost' field)
+    return path_feat['cost'] if path_feat else None
+
+
+def computeF1(struct_count):
+    if struct_count == 0:
+        # No obstruction or alteration in the continuity of sediment and wood transport upstream of the segment
         return 0
-
-    ratio = struct_count / length
-    if ratio <= 1:
+    if struct_count <= 1:
+        # Presence of at least one obstacle to the continuous flow of sediment and wood upstream of the segment
         return 2
-    elif ratio > 1:
+    elif struct_count > 1:
+        # Presence of more than one obstacle to the continuous flow of sediment and wood upstream of the segment
         return 4
